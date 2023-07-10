0997588f5d26890b73934df82877720ac703df6a
Michal Markiewicz <michalmarkiewicz0953@gmail.com>
2023-07-10 12:21:44 +0200

    Refactor

--- script.py
+++ script.py
@@ -8,17 +8,10 @@ url = f"https://it.pracuj.pl/?tt={technology}&jobBoardVersion=2"
 driver.get(url)
 
 
-def extract_text_from_elements(elements):
-    text_list = []
-    for element in elements:
-        text_list.append(element.text)
-    return text_list
-
-
 def get_job_titles():
     job_title_xpath = '//h3[@data-test="offer-title"]'
     elements = driver.find_elements(By.XPATH, job_title_xpath)
-    job_titles = extract_text_from_elements(elements)
+    job_titles = [element.text for element in elements]
     return job_titles
 
 
